0a43fb5e4fef4ae8f1fb957dd0456ccdb5e82854
STLucasConde <stlucasconde@gmail.com>
2021-03-08 14:58:36 -0300

    Tercer Commit 8/3

--- js/escritura.js
+++ js/escritura.js
@@ -1,7 +1,7 @@
 const typed = new Typed('.typed',{
     strings: [
-        '<i class="texto">de la mejor calidad.</i>',
-        '<i class="texto">al mejor precio.</i>',
+        '<i class="texto">calidad.</i>',
+        '<i class="texto">precio.</i>',
     ],
     typeSpeed: 90,
     startDelay:100,
